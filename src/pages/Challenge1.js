5eec73944647fe3180d91b444a888e6692f9adc6
xreyc <segumareyco@gmail.com>
2022-01-17 12:09:59 +0800

    api correction

--- src/pages/Challenge1.js
+++ src/pages/Challenge1.js
@@ -7,14 +7,14 @@ const Challenge1 = () => {
   useEffect(() => {
     const prepareData = async () => {
       // GET DATA
-      const getData = await fetch('https://reyc-testapp4.herokuapp.com/api/counter');
+      const getData = await fetch('https://reyc-testapp3.herokuapp.com/api/counter');
       const gdata = await getData.json();
       if(gdata.length !== 0) {
         setCounter({ id: gdata[0]._id, count: gdata[0].count });
       }
       else {
         // POST DATA
-        const postData = await fetch('https://reyc-testapp4.herokuapp.com/api/counter', 
+        const postData = await fetch('https://reyc-testapp3.herokuapp.com/api/counter', 
           {
             method: "POST",
             headers: {
@@ -35,7 +35,7 @@ const Challenge1 = () => {
   // UPDATE PER COUNT CHANGES
   useEffect(() => {
     const updateData = async () => {
-      await fetch('https://reyc-testapp4.herokuapp.com/api/counter/' + counter.id, 
+      await fetch('https://reyc-testapp3.herokuapp.com/api/counter/' + counter.id, 
         {
           method: "PUT",
           headers: {
